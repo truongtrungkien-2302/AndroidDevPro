df9c79b61019a923f6e7ab97e15ee630dc294412
Trương Trung Kiên <truongtrungkien2302@gmail.com>
2021-11-26 15:00:11 +0700

    Update

--- AndroidIntentRemakePersistence7/app/src/main/java/com/android45/androidintentremakepersistence7/IonClickContact.java
+++
@@ -1,7 +0,0 @@
-package com.android45.androidintentremakepersistence7;
-
-public interface IonClickContact {
-
-    void onClickName(String name,int position);
-    void onClickPhone(Contact contact);
-}
