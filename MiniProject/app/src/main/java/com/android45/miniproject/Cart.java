985e2f417edd9b2afe7db57b1afbe5b91ccfa8bf
Trương Trung Kiên <truongtrungkien2302@gmail.com>
2021-11-29 02:59:15 +0700

    Food

---
+++ MiniProject/app/src/main/java/com/android45/miniproject/Cart.java
@@ -0,0 +1,4 @@
+package com.android45.miniproject;
+
+public class Cart {
+}
--- MiniProject/app/src/main/java/com/android45/miniproject/MainActivity.java
+++ MiniProject/app/src/main/java/com/android45/miniproject/MainActivity.java
@@ -3,7 +3,11 @@ package com.android45.miniproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -12,21 +16,45 @@ public class MainActivity extends AppCompatActivity {
     ListView lvFood;
     List<FoodMenu> foodMenuList;
     FoodMenu foodMenu1, foodMenu2, foodMenu3, foodMenu4, foodMenu5;
+    ImageView imgAdd, imgRemove;
+    TextView tvCart;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        lvFood = findViewById(R.id.lvFood);
+        getUserName();
+        addFoodMenuList();
+//        foodAddAndRemove();
+    }
 
+    private void getUserName() {
+
+    }
+
+    private void addFoodMenuList() {
+        lvFood = findViewById(R.id.lvFood);
         foodMenuList = new ArrayList<>();
         foodMenuList.add(new FoodMenu(R.drawable.eggsand, "Egg Salad Sandwich", 4, "60.000"));
         foodMenuList.add(new FoodMenu(R.drawable.ham, "Ham & Cheese Sandwich", 5, "65.000"));
         foodMenuList.add(new FoodMenu(R.drawable.chicken, "Chicken Salad Sandwich", 4, "75.000"));
-        foodMenuList.add(new FoodMenu(R.drawable.turkey, "Avocado & Turkey Sandwich", 4.5F, "70.000"));
+        foodMenuList.add(new FoodMenu(R.drawable.turkey, "French fries & Turkey Sandwich", 4.5F, "70.000"));
         foodMenuList.add(new FoodMenu(R.drawable.beef, "Beef Rib Burger", 5, "80.000"));
 
         AdapterFood adapterFood = new AdapterFood(foodMenuList);
         lvFood.setAdapter(adapterFood);
     }
+
+//    private void foodAddAndRemove() {
+//        imgAdd = findViewById(R.id.imgAdd);
+//        imgRemove = findViewById(R.id.imgRemove);
+//        tvCart = findViewById(R.id.tvCart);
+//
+//        imgAdd.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                tvCart.setText(tvCart.getText().toString() + "1");
+//            }
+//        });
+//    }
 }
